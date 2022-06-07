c27b6a723b11eb56a4b21e60af5d9f49c8b2264f
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-06-07 20:33:43 -0300

    fixing sercurty breaches

--- api_pets/settings.py
+++ api_pets/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7e&133$5(v)_h*2=d6=a*_mdq_=(nbvehoe)w=^ch#oz+v0th)"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,4 +139,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "adoptpetpytho@gmail.com"
-EMAIL_HOST_PASSWORD = "uuhqcvniqyukyzzb"
+EMAIL_HOST_PASSWORD = ""
