476af2bd0df0589a21c6ffc237010c721b78e34a
MarcelloCavazza <marcellocavazzaoliveira@gmail.com>
2022-06-07 19:40:08 -0300

    created more 2 end points

--- adoption/views.py
+++ adoption/views.py
@@ -1,11 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST,HTTP_200_OK
 from .serializers import AdoptionSerializer
 from .email_service import sendEmailConfirmation
-
+from .models import Adoption
 
 class AdoptionList(APIView):
+    def get(self, request, format=None):
+        adoption = Adoption.objects.all()
+        serializer = AdoptionSerializer(adoption, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
     def post(self, request, format=None):
         serializer = AdoptionSerializer(data=request.data)
         if serializer.is_valid():
